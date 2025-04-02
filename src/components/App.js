4b600a3da7746ad3b8fa1a7f2ed55d59f274cfe6
Jake2508 <72077965+Jake2508@users.noreply.github.com>
2025-04-02 11:16:11 +0100

    Setup Add & Remove functionality
    - Field check on Add
    - Save/Local storage of saved contacts for web refresh
    - Added IDs to contact list to easily access entries
    - Remove functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import Header from "./Header"
 import AddContact from "./AddContact"
@@ -6,28 +7,42 @@ import ContactList from "./ContactList"
 
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
 
-  const contacts = [
-    {
-      id: "1",
-      name: "Jake",
-      email: "example@123.com",
-    },
-    {
-      id: "2",
-      name: "Max",
-      email: "Max@123.com",
-    }
+  // Load contacts from localStorage on initial render
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return savedContacts ? JSON.parse(savedContacts) : [];
+  });
 
-  ];
+  const addContactHandler = (contact) => {
+    const newContact = { id: uuidv4(), ...contact };
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    
+    setContacts(newContactList);
+  };
+
+  useEffect(() => {
+    if(contacts.length > 0) {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+    } 
+  }, [contacts]);
+
 
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
-      <ContactList contacts={contacts} />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
+
 
 export default App;
