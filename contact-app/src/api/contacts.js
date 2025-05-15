a0c10037b982647f31a9fddebd79b95e27661ddf
Jake2508 <72077965+Jake2508@users.noreply.github.com>
2025-05-15 13:01:58 +0100

    Added live backend url
    - Updated local (mocked backend) to actual live backend

--- contact-app/src/api/contacts.js
+++ contact-app/src/api/contacts.js
@@ -3,5 +3,6 @@ import axios from 'axios';
 
 export default axios.create({
     // We can change URL to use an actual server (instead of local)
-    baseURL:"http://localhost:3006/",
+    // baseURL:"http://localhost:3006/",
+    baseURL:"https://contact-app-fuwf.onrender.com/",
 })
